8c1cb1b318f23825f36a21c82036ed4cc147ba44
Luke Schaufuss <lukeschaufuss@gmail.com>
2023-07-09 16:28:20 +0100

    added error messages and some css

--- app.py
+++ app.py
@@ -12,11 +12,44 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Connect Database
+conn = sqlite3.connect("/Users/starkindustries/Desktop/project/webapp/intranet.db")
 
-@app.route("/login")
+# Create cursor
+cur = conn.cursor()
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        return None
+        
+        # Gets user input from form
+        email = request.form.get("email")
+        password = request.form.get("password")
+        
+        # Checks for input
+        if not email:
+            flash("Enter an email address")
+            return render_template("login.html")
+        
+        if not password:
+            flash("Enter your password")
+            return render_template("login.html")
+        
+        # Lookup in db
+        db_lookup = cur.execute("SELECT email, hash FROM users WHERE email = ?;", email)
+        
+        # Looks to see if they match a user
+        if not db_lookup["email"]:
+            flash("Email is not recognized")
+            return render_template("login.html")
+        
+        if not db_lookup["hash"]:
+            flash("Incorrect password")
+            return render_template("login.html")
+        
+        
+
+        return redirect("/")
     return render_template("login.html")
 
 @app.route("/register")
